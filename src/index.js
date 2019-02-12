4efaa40e2d795954dcfbc1bfd9db1a7d96ba0968
Manoj Singh Negi <justanothermanoj@gmail.com>
2019-02-13 03:33:38 +0530

    added useOnOutsideClick custom hook

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-const useOnOutsideClick = {}
+import { useRef, useEffect } from 'react'
+
+/**
+ * @param {function} func - Function to be called when click outside the component
+ * @returns {ref} the ref which will be attached to target element
+*/
+function useOnOutsideClick (func) {
+    const elementInstance = useRef(null)
+
+    const onDocumentClick = event => {
+        if (!elementInstance.current.contains(event.target)) {
+            func()
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('click', onDocumentClick)
+
+        return () => document.removeEventListener('click', onDocumentClick)
+    }, [])
+
+    return elementInstance
+}
 
 export default useOnOutsideClick
